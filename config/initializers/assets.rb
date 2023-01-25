3ce6cc731d948633ba870494a7794daadba5191c
Sophie Archer <hello@sophiearcher.co.uk>
2023-01-25 16:00:37 +0000

    Prepare rails app with external specs

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,6 +4,9 @@
 Rails.application.config.assets.version = "1.0"
 Rails.application.config.assets.paths << Rails.root.join("node_modules")
 
+Rails.application.config.assets.paths << Rails.root.join("node_modules")
+
+
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
 
